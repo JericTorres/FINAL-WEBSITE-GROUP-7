f2ba239fac552e1d03e42b8642c88fc5d75229cf
JericTorres <jerictorrjerictorres456@gmail.com>
2025-01-22 13:00:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start(); // Start the session
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Escape the Nature!</title>
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
